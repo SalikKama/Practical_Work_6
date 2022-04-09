c3901f90de7f77d73f5ebf25b8ae2dc30676d7f0
«Kamila» <milo4-ka01@yandex.ru>
2022-04-09 19:13:29 +0300

    Задание 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void FillMatrix(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++ )
+        for(int j = 0; j < matr.GetLength(1); j++ )
+            matr[i,j] = i + j;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++ )
+    {
+        for(int j = 0; j < matr.GetLength(1); j++ )
+            Console.Write(matr[i,j] + " ");
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[5,6];
+FillMatrix(matrix);
+PrintMatrix(matrix);
